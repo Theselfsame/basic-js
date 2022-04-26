4c107e8e44204490e7386ebc67ac11409dc508bc
Siarhei Matsiushkin <matiushkin.s@gmail.com>
2022-04-26 21:04:39 +0300

    feat: add carbone-date code

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,23 +3,19 @@ const { NotImplementedError } = require('../extensions/index.js');
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
-/**
- * Determine the age of archeological find by using
- * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
- * @return {Number | Boolean} calculated age in years or false
- * in case of incorrect sampleActivity
- *
- * @example
- * 
- * dateSample('1') => 22387
- * dateSample('WOOT!') => false
- *
- */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity ) {
+  if (!parseFloat(sampleActivity)) {
+    return false;
+  }
+  if(typeof sampleActivity != 'string') {
+    return false;
+  }
+  if (parseFloat(sampleActivity) <= 0 || parseFloat(sampleActivity) > 15) {
+    return false
+  }
+  const k = 0.693 / HALF_LIFE_PERIOD;
+
+  return Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / k)
 }
 
 module.exports = {
